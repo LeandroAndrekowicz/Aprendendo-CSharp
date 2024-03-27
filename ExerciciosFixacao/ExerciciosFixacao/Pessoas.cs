cdca88310406f504ebb0159e2ed87eb169fa359c
Leandro Andrekowicz <103007824+LeandroAndrekowicz@users.noreply.github.com>
2024-03-27 17:57:54 -0300

    Aprendendo Classes

---
+++ ExerciciosFixacao/ExerciciosFixacao/Pessoas.cs
@@ -0,0 +1,8 @@
+﻿namespace Pessoas
+{
+    internal class Pessoas
+    {
+        public string Nome;
+        public int Idade;
+    }
+}
---
+++ ExerciciosFixacao/ExerciciosFixacao/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Pessoas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoas pessoa1, pessoa2;
+
+            pessoa1 = new Pessoas();
+
+            pessoa2 = new Pessoas();
+
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.Write("Nome: ");
+            pessoa1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoa1.Idade = int.Parse(Console.ReadLine());
+
+            Console.Write("Dados da segunda pessoa: ");
+            pessoa2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoa2.Idade = int.Parse(Console.ReadLine());
+
+            if (pessoa1.Idade == pessoa2.Idade)
+            {
+                Console.WriteLine("{0} tem a mesma idade de {1}", pessoa1.Nome, pessoa2.Nome);
+            }
+
+            else if(pessoa1.Idade > pessoa2.Idade)
+            {
+                Console.WriteLine("{0} é mais velha que {1}", pessoa1.Nome, pessoa2.Nome);
+            }
+
+            else
+            {
+                Console.WriteLine("{0} é mais velha que {1}", pessoa2.Nome, pessoa1.Nome);
+            }
+
+        }
+    }
+}
